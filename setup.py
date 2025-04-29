07e9aa36081e2eef3416ee716eb9f905975df2d8
Sebastian Graf <61550902+Scorn123@users.noreply.github.com>
2025-04-29 09:22:41 +0000

    Kind of finishing

--- setup.py
+++ setup.py
@@ -1,1 +1,20 @@
-# TODO
+from setuptools import setup, find_packages
+
+setup(
+    name="do-dpc",
+    version="1.0.0",
+    author="Sebastian Graf",
+    author_email="Sebastian Graf",
+    description="Framework software package for the Data-Driven Predictive Control (DPC) algorithm with visual examples.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/do-dpc/do-dpc",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.12',
+    install_requires=load_requirements(),
+)
